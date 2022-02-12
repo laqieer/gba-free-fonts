03c2da0082daee42c969f59ca57dc3d6245984f1
laqieer <laqieer@126.com>
2022-02-12 21:36:48 +0800

    Unifont.
    https://github.com/GValiente/butano/pull/18

--- examples/butano/src/main.cpp
+++ examples/butano/src/main.cpp
@@ -5,31 +5,57 @@
 
 #include "bn_core.h"
 #include "bn_math.h"
+#include "bn_keypad.h"
 #include "bn_sprite_ptr.h"
 #include "bn_bg_palettes.h"
 #include "bn_string_view.h"
 #include "bn_sprite_text_generator.h"
 
 #include "hanamin_a_sprite_font.h"
+#include "unifont_sprite_font.h"
 
 namespace
 {
-    void common_font_text_scene()
+    void hanamin_a_text_scene()
     {
         bn::sprite_text_generator text_generator(hanamin_a_sprite_font);
         text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 128>text_sprites;
-        text_generator.generate(0, -60, "English Français русский язык", text_sprites);
-        text_generator.generate(0, -40, "简体中文 繁體中文（台灣）", text_sprites);
-        text_generator.generate(0, -20, "繁體中文（香港）Монголхэл", text_sprites);
-        text_generator.generate(0, 0, "日本語（にほんご ）", text_sprites);
-        text_generator.generate(0, 20, "Tiếng Việt ภาษาไทย", text_sprites);
-        text_generator.generate(0, 40, "Македонски Ўзбекча", text_sprites);
-        text_generator.generate(0, 60, "☺♨➑☆★♡✂♪♫☑", text_sprites);
+        text_generator.generate(0, -68, "- HanaMin A / 花園明朝 / 花园明朝 -", text_sprites);
+        text_generator.generate(0, -48, "English Français русский язык", text_sprites);
+        text_generator.generate(0, -28, "简体中文 繁體中文（台灣）", text_sprites);
+        text_generator.generate(0, -8, "繁體中文（香港）Монголхэл", text_sprites);
+        text_generator.generate(0, 12, "日本語（にほんご ）", text_sprites);
+        text_generator.generate(0, 32, "Tiếng Việt ภาษาไทย", text_sprites);
+        text_generator.generate(0, 52, "Македонски Ўзбекча", text_sprites);
+        text_generator.generate(0, 72, "☺♨➑☆★♡✂♪♫☑", text_sprites);
 
+        while(! bn::keypad::start_pressed())
+        {
+            bn::core::update();
+        }
+    }
 
-        bn::core::update();
+    void unifont_text_scene()
+    {
+        bn::sprite_text_generator text_generator(unifont_sprite_font);
+        text_generator.set_center_alignment();
+
+        bn::vector<bn::sprite_ptr, 128>text_sprites;
+        text_generator.generate(0, -68, "- Unifont -", text_sprites);
+        text_generator.generate(0, -48, "English Français русский язык", text_sprites);
+        text_generator.generate(0, -28, "简体中文 繁體中文（台灣）", text_sprites);
+        text_generator.generate(0, -8, "繁體中文（香港）Монголхэл", text_sprites);
+        text_generator.generate(0, 12, "日本語（にほんご ）한국어", text_sprites);
+        text_generator.generate(0, 32, "Tiếng Việt ภาษาไทย", text_sprites);
+        text_generator.generate(0, 52, "Македонски Ўзбекча", text_sprites);
+        text_generator.generate(0, 72, "☺♨➑☆★♡✂♪♫☑", text_sprites);
+
+        while(! bn::keypad::start_pressed())
+        {
+            bn::core::update();
+        }
     }
 }
 
@@ -38,5 +64,13 @@ int main()
     bn::core::init();
 
     bn::bg_palettes::set_transparent_color(bn::color(16, 16, 16));
-    common_font_text_scene();
+
+    while(true)
+    {
+        hanamin_a_text_scene();
+        bn::core::update();
+
+        unifont_text_scene();
+        bn::core::update();
+    }
 }
