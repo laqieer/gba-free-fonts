5de19fac708d8b70eb23661acb6a15df426e12d5
laqieer <laqieer@126.com>
2022-02-13 23:58:57 +0800

    BabelStone Han.

--- examples/butano/src/main.cpp
+++ examples/butano/src/main.cpp
@@ -13,6 +13,7 @@
 
 #include "hanamin_sprite_font.h"
 #include "unifont_sprite_font.h"
+#include "babelstone_han_sprite_font.h"
 #include "source_han_serif_jp_sprite_font.h"
 #include "source_han_sans_jp_sprite_font.h"
 #include "source_han_mono_jp_sprite_font.h"
@@ -54,6 +55,27 @@ namespace
         text_generator.generate(0, 32, "Tiếng Việt ภาษาไทย", text_sprites);
         text_generator.generate(0, 52, "Македонски Ўзбекча", text_sprites);
         text_generator.generate(0, 72, "☺♨➑☆★♡✂♪♫☑", text_sprites);
+
+        while(! bn::keypad::start_pressed())
+        {
+            bn::core::update();
+        }
+    }
+
+    void babelstone_han_text_scene()
+    {
+        bn::sprite_text_generator text_generator(babelstone_han_sprite_font);
+        text_generator.set_center_alignment();
+
+        bn::vector<bn::sprite_ptr, 128>text_sprites;
+        text_generator.generate(0, -68, "- BabelStone Han -", text_sprites);
+        text_generator.generate(0, -48, "English 简体中文 繁體中文（台灣）", text_sprites);
+        text_generator.generate(0, -28, "繁體中文（香港）日本語（にほんご）", text_sprites);
+        text_generator.generate(0, -8, "基本:义礼说选 扩A: 㡛㬐㖧䵍", text_sprites);
+        text_generator.generate(0, 12, "扩B:𣚣䶅𪘆𦞕 扩C:𪢨𪪖𫔺𫜴", text_sprites);
+        text_generator.generate(0, 32, "扩D:𫝀𫞂𫝻𫠝 扩E:𫠫𬚚𬳕𬺡", text_sprites);
+        text_generator.generate(0, 52, "扩F:𬺰𬺱𮯟𮯠 扩G:𰀀𰀁𰀂𰀃", text_sprites);
+        text_generator.generate(0, 72, "兼容:豈更車賈 增补:乁𠄢𠕋𠨬", text_sprites);
 
         while(! bn::keypad::start_pressed())
         {
@@ -137,6 +159,9 @@ int main()
         bn::core::update();
 
         unifont_text_scene();
+        bn::core::update();
+
+        babelstone_han_text_scene();
         bn::core::update();
 
         source_han_serif_text_scene();
