89e29216bcde212ada1b0e34a89b7c5f3c62768f
laqieer <laqieer@126.com>
2022-02-13 20:03:13 +0800

    Add HanaMin B to HanaMin font. 60,317 characters ranged from Ext. B to Ext. F added.

--- examples/butano/src/main.cpp
+++ examples/butano/src/main.cpp
@@ -11,23 +11,23 @@
 #include "bn_string_view.h"
 #include "bn_sprite_text_generator.h"
 
-#include "hanamin_a_sprite_font.h"
+#include "hanamin_sprite_font.h"
 #include "unifont_sprite_font.h"
 #include "source_han_serif_jp_sprite_font.h"
 
 namespace
 {
-    void hanamin_a_text_scene()
+    void hanamin_text_scene()
     {
-        bn::sprite_text_generator text_generator(hanamin_a_sprite_font);
+        bn::sprite_text_generator text_generator(hanamin_sprite_font);
         text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 128>text_sprites;
-        text_generator.generate(0, -68, "- HanaMin A / 花園明朝 / 花园明朝 -", text_sprites);
+        text_generator.generate(0, -68, "- HanaMin / 花園明朝 / 花园明朝 -", text_sprites);
         text_generator.generate(0, -48, "English Français русский язык", text_sprites);
         text_generator.generate(0, -28, "简体中文 繁體中文（台灣）", text_sprites);
         text_generator.generate(0, -8, "繁體中文（香港）Монголхэл", text_sprites);
-        text_generator.generate(0, 12, "日本語（にほんご ）", text_sprites);
+        text_generator.generate(0, 12, "日本語（にほんご ）𫝀𫠫𬺰𬺱", text_sprites);
         text_generator.generate(0, 32, "Tiếng Việt ภาษาไทย", text_sprites);
         text_generator.generate(0, 52, "Македонски Ўзбекча", text_sprites);
         text_generator.generate(0, 72, "☺♨➑☆★♡✂♪♫☑", text_sprites);
@@ -89,7 +89,7 @@ int main()
 
     while(true)
     {
-        hanamin_a_text_scene();
+        hanamin_text_scene();
         bn::core::update();
 
         unifont_text_scene();
