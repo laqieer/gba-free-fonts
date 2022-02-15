e2bae006e569d6a930eb197e0935c2121034a2e7
laqieer <laqieer@126.com>
2022-02-15 12:34:45 +0800

    Filter comments when counting characters from header files and assembly source files.

--- tools/butano/butano_fonts_tool.py
+++ tools/butano/butano_fonts_tool.py
@@ -51,7 +51,8 @@ def list_texts_files(texts_paths):
 def process_texts_files(texts_file_paths, characters_file_path):
     global unique_characters
     for texts_file_path in texts_file_paths:
-        if texts_file_path.lower().endswith('.c') or texts_file_path.lower().endswith('.cpp'):
+        texts_file_ext = os.path.splitext(texts_file_path.lower())[1]
+        if texts_file_ext in ('.c', '.cpp', '.cc', '.cxx', '.c++', '.h', '.hpp', '.hh', '.hxx', '.h++', '.s', '.inc', '.asm'):
             text = subprocess.check_output([os.environ['DEVKITARM'] + '/bin/arm-none-eabi-cpp', '-fpreprocessed', texts_file_path],shell=True).decode('utf-8')
             for char in text:
                 unique_characters.add(char)
